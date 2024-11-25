d8c4aa026f004fba1a62940424f9dec5976a7f49
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2024-11-25 12:41:01 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     runGame("addition");
+
 })
 
 /**
@@ -33,7 +34,9 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
       displayAdditionQuestion(num1, num2);
-    } else {
+    }  else if(gameType === "multiply")  {
+      displayMultiplyQuestion(num1, num2);
+    }  else {
       alert(`Unknown game type: ${gameType}`);
       throw `Unknown game type: ${gameType}. Aborting!` 
 
@@ -75,6 +78,8 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
     return [operand1 + operand2, "addition"];
+  }  else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
   } else { 
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -110,7 +115,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "x";
 
 }
 
